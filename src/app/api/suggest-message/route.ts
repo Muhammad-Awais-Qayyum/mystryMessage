54933b569405827f2d7c765d7d1df276b7b17f1b
Muhammad Awais Qayyum <awaisquyyum010@gmail.com>
2024-05-29 12:31:01 -0700

    updated

--- src/app/api/suggest-message/route.ts
+++ src/app/api/suggest-message/route.ts
@@ -1,26 +1,27 @@
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 
-// Create an OpenAI API client (that's edge friendly!)
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
+// Create an OpenAI API client
+// but configure it to point to fireworks.ai
+const fireworks = new OpenAI({
+    apiKey: process.env.FIREWORKS_API_KEY || '',
+    baseURL: 'https://api.fireworks.ai/inference/v1',
 });
 
-export const dynamic = 'force-dynamic';
-
 export async function POST(req: Request) {
+    // Extract the `messages` from the body of the request
     try {
         const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What's a hobby you've recently started? | | If you could have dinner with any historical figure, who would it be? || What's a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment."
 
-        // Ask OpenAI for a streaming chat completion given the prompt
-        const response = await openai.completions.create({
-            model: 'gpt-3.5-turbo-instruct',
+        // Ask Fireworks for a streaming chat completion using Llama 2 70b model
+        // @see https://app.fireworks.ai/models/fireworks/llama-v2-70b-chat
+        const response = await fireworks.completions.create({
+            model: 'accounts/fireworks/models/llama-v2-70b-chat',
             stream: true,
-            max_tokens:400,
+            max_tokens: 300,
             prompt,
         });
-
-        // Convert the response into a friendly text-stream
+        // Convert the response into a friendly text-stream.
         const stream = OpenAIStream(response);
         // Respond with the stream
         return new StreamingTextResponse(stream);
